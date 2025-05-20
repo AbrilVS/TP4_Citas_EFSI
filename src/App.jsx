41f60e83da706169383825b88efe093935b1a6c3
ABRIL VARELA SICA <48464274@ort>
2025-05-20 08:41:43 -0300

    citas2

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,35 @@
-import React, {useState} from 'react'
-import  Formulario  from './Formulario.jsx'
-import './App.css'
-import  Listado  from './Listado.jsx'
-import './Formulario.css'
-import './Listado.css'
-import './Cita.css'
+import React, { useEffect, useState } from 'react';
+import Formulario from './Formulario.jsx';
+import Listado from './Listado.jsx';
+import './App.css';
+import './Formulario.css';
+import './Listado.css';
+import './Cita.css';
 
 function App() {
-const [citas, setCitas]=useState([]);
+  const [citas, setCitas] = useState(() => {
+    const citasGuardadas = localStorage.getItem('Citas');
+    if(citasGuardadas!=null){
+      return JSON.parse(citasGuardadas)
+    }else{
+      return []
+    }
+  });
+
+
+  useEffect(() => {
+    localStorage.setItem('Citas', JSON.stringify(citas));
+  }, [citas]);
 
   return (
     <div className="container">
       <h1>Administra tus citas</h1>
       <div className="row">
-        
-        <Formulario setCitas={setCitas} citas={citas}/>
-        <Listado listado={citas} setCitas={setCitas}/>
+        <Formulario setCitas={setCitas} citas={citas} />
+        <Listado listado={citas} setCitas={setCitas} />
       </div>
     </div>
-
-  )
+  );
 }
 
-export default App
+export default App;
